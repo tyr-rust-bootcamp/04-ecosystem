e0e063f6783e322626ecad37b804f733a6260f9a
Tyr Chen <tyr.chen@gmail.com>
2024-04-07 11:54:30 -0700

    feature: introduce tracing & tracing-subscriber

---
+++ examples/axum_tracing.rs
@@ -0,0 +1,61 @@
+use std::time::Duration;
+
+use axum::{routing::get, Router};
+use tokio::{
+    net::TcpListener,
+    time::{sleep, Instant},
+};
+use tracing::{debug, info, instrument, level_filters::LevelFilter, warn};
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+    Layer,
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let file_appender = tracing_appender::rolling::daily("/tmp/logs", "ecosystem.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let console = fmt::Layer::new()
+        .with_span_events(FmtSpan::CLOSE)
+        .pretty()
+        .with_filter(LevelFilter::DEBUG);
+
+    let file = fmt::Layer::new()
+        .with_writer(non_blocking)
+        .pretty()
+        .with_filter(LevelFilter::INFO);
+
+    tracing_subscriber::registry()
+        .with(console)
+        .with(file)
+        .init();
+
+    let addr = "0.0.0.0:8080";
+    let app = Router::new().route("/", get(index_handler));
+
+    let listener = TcpListener::bind(addr).await?;
+    info!("Starting server on {}", addr);
+    axum::serve(listener, app.into_make_service()).await?;
+    Ok(())
+}
+
+#[instrument]
+async fn index_handler() -> &'static str {
+    debug!("index handler started");
+    sleep(Duration::from_millis(10)).await;
+    let ret = long_task().await;
+    info!(http.status = 200, "index handler completed");
+    ret
+}
+
+#[instrument]
+async fn long_task() -> &'static str {
+    let start = Instant::now();
+    sleep(Duration::from_millis(112)).await;
+    let elapsed = start.elapsed().as_millis() as u64;
+    warn!(app.task_duration = elapsed, "task takes too long");
+    "Hello, World!"
+}
--- examples/err.rs
+++ examples/err.rs
@@ -1,6 +1,32 @@
 use anyhow::Context;
-use ecosystem::MyError;
 use std::{fs, mem::size_of, num::ParseIntError};
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Parse error: {0}")]
+    Parse(#[from] std::num::ParseIntError),
+    #[error("Serialize json error: {0}")]
+    Serialize(#[from] serde_json::Error),
+
+    #[error("Error: {0:?}")]
+    BigError(Box<BigError>),
+
+    #[error("Custom error: {0}")]
+    Custom(String),
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub struct BigError {
+    a: String,
+    b: Vec<String>,
+    c: [u8; 64],
+    d: u64,
+}
 
 fn main() -> Result<(), anyhow::Error> {
     println!("size of anyhow::Error is {}", size_of::<anyhow::Error>());
